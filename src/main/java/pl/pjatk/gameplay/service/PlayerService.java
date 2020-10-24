ecdb6b98657a5f1835cb7855f5d92e10ef0fd582
Michal Wadas <michal.wadas@hycom.pl>
2020-10-24 20:48:29 +0200

    getMapping & postMapping

--- src/main/java/pl/pjatk/gameplay/service/PlayerService.java
+++ src/main/java/pl/pjatk/gameplay/service/PlayerService.java
@@ -2,15 +2,40 @@ package pl.pjatk.gameplay.service;
 
 import org.springframework.stereotype.Service;
 import pl.pjatk.gameplay.model.Player;
+import pl.pjatk.gameplay.repository.PlayerRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerService {
 
+    private PlayerRepository playerRepository;
+
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public Player save(Player player) {
+        return playerRepository.save(player);
+    }
+
+    private List<Player> players = List.of(new Player(1L, "DDD2 ", 303, 23, 140),
+            new Player(2L, "John", 23, 30, 334));
+
     public List<Player> findAll() {
-        return List.of(new Player(4, "dupa", 400, 200, 500),
-                new Player());
+        return playerRepository.findAll();
+    }
+
+    public Optional<Player> findById(int id) {
+        List<Player> players = new ArrayList<>();
+        players.add(new Player(5L, "okon", 500, 500, 330));
+        players.add(new Player(3L, "dpa", 200, 500, 330));
+
+        return players.stream()
+                .filter(player -> player.getId() == id)
+                .findFirst();
     }
 }
 
